6c90ff786ddad68545bc3c4f9308654b9199de75
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-02-28 20:20:01 +0900

    速度計の実装

--- project/src/main/java/com/github/graycat27/flightHUDmod/guiComponent/SpeedMeter.java
+++ project/src/main/java/com/github/graycat27/flightHUDmod/guiComponent/SpeedMeter.java
@@ -1,6 +1,9 @@
 package com.github.graycat27.flightHUDmod.guiComponent;
 
 import com.github.graycat27.flightHUDmod.FlightHUDMod;
+import com.github.graycat27.flightHUDmod.consts.TextHorizontalPosition;
+import com.github.graycat27.flightHUDmod.guiDisplay.IGuiValueDisplay;
+import com.github.graycat27.flightHUDmod.guiDisplay.TextDisplay;
 import com.github.graycat27.flightHUDmod.unit.Speed;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
@@ -13,13 +16,54 @@ public class SpeedMeter extends GuiComponent {
 
     private Speed speed = null;
 
+    private IGuiValueDisplay horizonSpeedTextDisplay;
+    private IGuiValueDisplay verticalSpeedTextDisplay;
+    private IGuiValueDisplay actualSpeedTextDisplay;
+
     public SpeedMeter(){
         super();
+        FlightHUDMod.getLogger().info("SpeedMeter has constructed");
+
+        initDisplayComponent();
+    }
+
+    private void initDisplayComponent(){
+        Minecraft mc = Minecraft.getInstance();
+        int windowWidth = mc.getMainWindow().getScaledWidth();
+        int windowHeight = mc.getMainWindow().getScaledHeight();
+
+        int posX = windowWidth * 2/5;
+        int basePosY = windowHeight / 2;
+        int width = mc.fontRenderer.getStringWidth("1,234.321");
+        int height = mc.fontRenderer.FONT_HEIGHT;
+        boolean isVisible = this.isDisplayed();
+        String text = "";
+        TextHorizontalPosition hPos = TextHorizontalPosition.RIGHT;
+
+        //init and display
+        horizonSpeedTextDisplay = new TextDisplay(posX + (width/3), basePosY, width,height, isVisible, text, hPos);
+
+        if(speed != null && speed.getVerticalSpeed() >= 0){
+            //going UP
+            verticalSpeedTextDisplay = new TextDisplay(posX, basePosY-height,
+                    width, height, isVisible, text, hPos);
+            actualSpeedTextDisplay = new TextDisplay(posX + width*2/3, basePosY - (int)(2.5*height),
+                    width, height, isVisible, text, hPos);
+        }else{
+            //going DOWN
+            verticalSpeedTextDisplay = new TextDisplay(posX, basePosY+height,
+                    width, height, isVisible, text, hPos);
+            actualSpeedTextDisplay = new TextDisplay(posX + width*2/3, basePosY + (int)(2.5*height),
+                    width, height, isVisible, text, hPos);
+        }
+
     }
 
     @Override
     protected void drawDisplayComponent(){
-        throw new UnsupportedOperationException();
+        horizonSpeedTextDisplay.setVisible(true);
+        verticalSpeedTextDisplay.setVisible(true);
+        actualSpeedTextDisplay.setVisible(true);
     }
 
     @Override
@@ -30,9 +74,16 @@ public class SpeedMeter extends GuiComponent {
         }
 
         speed = new Speed(player);
-
         FlightHUDMod.getLogger().debug(speed);
 
+        //表示位置を水平位置から上下させるため、都度init
+        initDisplayComponent();
+
+        horizonSpeedTextDisplay.setDispValue(speed.getHorizonSpeedValStr() +"→");
+        String verticalStr = speed.getVerticalSpeedValStr();
+        verticalStr += (speed.getVerticalSpeed()>=0 ? "↑": "↓");
+        verticalSpeedTextDisplay.setDispValue(verticalStr);
+        actualSpeedTextDisplay.setDispValue(speed.getActualSpeedValStr());
     }
 
     @Override
