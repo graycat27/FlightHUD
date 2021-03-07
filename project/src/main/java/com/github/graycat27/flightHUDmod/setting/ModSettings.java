6422afe5bfb63e030e1b8d5f8393bf6f9e03f139
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-03-07 09:15:11 +0900

    表示位置をプロパティファイルに読み書きするように修正

--- project/src/main/java/com/github/graycat27/flightHUDmod/setting/ModSettings.java
+++ project/src/main/java/com/github/graycat27/flightHUDmod/setting/ModSettings.java
@@ -15,6 +15,15 @@ public class ModSettings {
 
     private File configFile;
 
+    private static class DefaultValue{
+        static boolean showHUD = true;
+        static int interval = 15;
+        static double positionCompass = 0.3d;
+        static double positionHeight = 0.7d;
+        static double positionPitch = 0.5d;
+        static double positionSpeed = 0.3d;
+    }
+
     /** FlightHUDの表示ON/OFF */
     private boolean showHUD = true;
     public boolean isShowHud(){
@@ -44,6 +53,24 @@ public class ModSettings {
             interval -= CHANGE_INTERVAL;
             saveConfigFile();
         }
+    }
+
+    /* 表示位置 */
+    private double positionCompass = DefaultValue.positionCompass;
+    private double positionHeight = DefaultValue.positionHeight;
+    private double positionPitch = DefaultValue.positionPitch;
+    private double positionSpeed = DefaultValue.positionSpeed;
+    public double getPositionCompass(){
+        return positionCompass;
+    }
+    public double getPositionHeight(){
+        return positionHeight;
+    }
+    public double getPositionPitch(){
+        return positionPitch;
+    }
+    public double getPositionSpeed(){
+        return positionSpeed;
     }
 
     /** コンストラクタ */
@@ -85,6 +112,50 @@ public class ModSettings {
             }
             this.interval = intervalInt;
 
+            //position
+            {
+                //position.compass
+                String compassPosStr = prop.getProperty(ConfigFile.COMPASS);
+                double compassDbl = DefaultValue.positionCompass;
+                try {
+                    compassDbl = Double.parseDouble(compassPosStr);
+                } catch (NullPointerException | NumberFormatException e) {
+                    lg.warn("compass position is wrong value:" + compassPosStr);
+                    successToRead = false;
+                }
+                this.positionCompass = compassDbl;
+                //position.height
+                String heightPosStr = prop.getProperty(ConfigFile.HEIGHT);
+                double heightDbl = DefaultValue.positionHeight;
+                try {
+                    heightDbl = Double.parseDouble(heightPosStr);
+                } catch (NullPointerException | NumberFormatException e) {
+                    lg.warn("height position is wrong value:" + heightPosStr);
+                    successToRead = false;
+                }
+                this.positionHeight = heightDbl;
+                //position.pitch
+                String pitchPosStr = prop.getProperty(ConfigFile.PITCH);
+                double pitchDbl = DefaultValue.positionPitch;
+                try {
+                    pitchDbl = Double.parseDouble(pitchPosStr);
+                } catch (NullPointerException | NumberFormatException e) {
+                    lg.warn("pitch position is wrong value:" + pitchPosStr);
+                    successToRead = false;
+                }
+                this.positionPitch = pitchDbl;
+                //position.speed
+                String speedPosStr = prop.getProperty(ConfigFile.SPEED);
+                double speedDbl = DefaultValue.positionSpeed;
+                try {
+                    speedDbl = Double.parseDouble(speedPosStr);
+                } catch (NullPointerException | NumberFormatException e) {
+                    lg.warn("speed position is wrong value:" + speedPosStr);
+                    successToRead = false;
+                }
+                this.positionSpeed = speedDbl;
+            }
+
         }catch(FileNotFoundException e){
             FlightHUDMod.getLogger().info("config file not found. create and save default props.");
             successToRead = false;
@@ -121,6 +192,10 @@ public class ModSettings {
 
             writer.println(ConfigFile.SHOW + ":" + this.showHUD);
             writer.println(ConfigFile.PITCH_INTERVAL +":"+ this.interval);
+            writer.println(ConfigFile.COMPASS +":"+ this.positionCompass);
+            writer.println(ConfigFile.HEIGHT +":"+ this.positionHeight);
+            writer.println(ConfigFile.PITCH +":"+ this.positionPitch);
+            writer.println(ConfigFile.SPEED +":"+ this.positionSpeed);
 
         }
         catch(IOException e){
