48ef8883a419431febd7ce89445b403a3159ceb3
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-03-07 15:24:40 +0900

    merge ed94c39c29db97952d78109e29f435a02e2faef0

--- project/src/main/java/com/github/graycat27/flightHUDmod/consts/CompassScaleValue.java
+++ project/src/main/java/com/github/graycat27/flightHUDmod/consts/CompassScaleValue.java
@@ -4,28 +4,30 @@ import com.github.graycat27.flightHUDmod.unit.Direction;
 
 public enum CompassScaleValue {
     N(360),
-    NNE(23),
+    NNE(22.5),
     NE(45),
-    ENE(90-23),
+    ENE(90-22.5),
     E(90),
-    ESE(90+23),
+    ESE(90+22.5),
     SE(135),
-    SSE(180-23),
+    SSE(180-22.5),
     S(180),
-    SSW(180+23),
+    SSW(180+22.5),
     SW(225),
-    WSW(270-23),
+    WSW(270-22.5),
     W(270),
-    WNW(270+23),
-    NW(315);
+    WNW(270+22.5),
+    NW(315),
+    NNW(360-22.5)
+    ;
 
-    private int degrees;
+    private double degrees;
 
-    CompassScaleValue(int degrees){
+    CompassScaleValue(double degrees){
         this.degrees = degrees;
     }
 
-    public int value(){
+    public double value(){
         return degrees;
     }
 
@@ -54,7 +56,7 @@ public enum CompassScaleValue {
         return null;
     }
 
-    public static CompassScaleValue getByDegrees(int degrees){
+    public static CompassScaleValue getByDegrees(double degrees){
         for(CompassScaleValue val : CompassScaleValue.values()){
             if(val.degrees == degrees){
                 return val;
