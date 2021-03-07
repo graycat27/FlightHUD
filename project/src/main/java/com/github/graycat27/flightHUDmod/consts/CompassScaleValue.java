c6067072d407444d40c78adb6cc5e8e88f12ed4b
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-03-07 19:29:46 +0900

    remove un-need method

--- project/src/main/java/com/github/graycat27/flightHUDmod/consts/CompassScaleValue.java
+++ project/src/main/java/com/github/graycat27/flightHUDmod/consts/CompassScaleValue.java
@@ -31,31 +31,6 @@ public enum CompassScaleValue {
         return degrees;
     }
 
-    public static CompassScaleValue getByDegrees(int degrees, int range){
-        for(CompassScaleValue val : CompassScaleValue.values()){
-            while(degrees <= Direction.MIN_VAL){
-                degrees += Direction.ROUND;
-            }
-            while(Direction.ROUND < degrees){
-                degrees -= Direction.ROUND;
-            }
-            if(degrees - range <= Direction.MIN_VAL){
-                if(degrees - range + Direction.ROUND < val.degrees || val.degrees < degrees + range){
-                    return val;
-                }
-            }
-            if(Direction.ROUND < degrees + range){
-                if(degrees - range < val.degrees || val.degrees < degrees + range - Direction.ROUND){
-                    return val;
-                }
-            }
-            if (degrees - range < val.degrees && val.degrees < degrees + range) {
-                return val;
-            }
-        }
-        return null;
-    }
-
     public static CompassScaleValue getByDegrees(double degrees){
         for(CompassScaleValue val : CompassScaleValue.values()){
             if(val.degrees == degrees){
